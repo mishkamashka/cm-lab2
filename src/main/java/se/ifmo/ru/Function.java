68d12b42e34497b3eac430687a28a3375b49e3f5
Mariia <just-another-log-in@yandex.ru>
2018-09-27 15:39:30 +0300

    add Function structure

--- src/main/java/se/ifmo/ru/Function.java
+++ src/main/java/se/ifmo/ru/Function.java
@@ -1,4 +1,37 @@
 package se.ifmo.ru;
 
 public enum Function {
+
+    FIRST(""), SECOND(""), THIRD(""), FORTH(""), FIFTH("");
+
+    private String function;
+
+    Function(String function) {
+        this.function = function;
+    }
+
+    public double calculateFunction(double x) {
+        switch (this) {
+            case FIRST:
+                //return some func calculation
+                break;
+            case SECOND:
+                //return some func calculation
+                break;
+            case THIRD:
+                //return some func calculation
+                break;
+            case FORTH:
+                //return some func calculation
+                break;
+            case FIFTH:
+                //return some func calculation
+                break;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.function;
+    }
 }
