35c3a0f7d6c54906299dd5da30e9cbead795c80a
Mariia Anisimova <just-another-log-in@yandex.ru>
2018-11-08 14:57:23 +0300

    add calculate method

--- src/main/java/se/ifmo/ru/SimpsonCalculator.java
+++ src/main/java/se/ifmo/ru/SimpsonCalculator.java
@@ -5,7 +5,10 @@ public class SimpsonCalculator {
     private Function function;
     private double bottomLimit;
     private double topLimit;
-    private double e;
+    private double accuracy;
+    private double p;
+    private double uac;
+    private double result;
 
     public Function getFunction() {
         return function;
@@ -19,9 +22,21 @@ public class SimpsonCalculator {
         return topLimit;
     }
 
+    public double getP() {
+        return p;
+    }
+
+    public double getUac() {
+        return uac;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
     SimpsonCalculator(Function function, double bottomLimit, double topLimit, double accuracy) {
         this.function = function;
-        this.e = accuracy;
+        this.accuracy = accuracy;
         if (bottomLimit > topLimit) {
             this.bottomLimit = topLimit;
             this.topLimit = bottomLimit;
@@ -31,11 +46,46 @@ public class SimpsonCalculator {
         }
     }
 
-    public void calculate() {
-        for (int i = 4; i < 1000; i += 2) {
+    public int calculate() {
+        double i1;
+        double i2;
+        double h;
+        if (topLimit != bottomLimit) {
+            for (int n = 4; n <= 10000; n += 2) {
+                double sum1 = 0;
+                double sum2 = 0;
+                h = (topLimit - bottomLimit) / n;
+                for (int i = 1; i < n; i++) {
+                    sum1 += 4 * function.calculateFunction(bottomLimit + i * h);
+                    i++;
+                    sum1 += 2 * function.calculateFunction(bottomLimit + i * h);
+                }
+                i1 = (sum1 + function.calculateFunction(bottomLimit) - function.calculateFunction(topLimit) * h / 3);
 
+                h = (topLimit - bottomLimit) / (2 * n);
+                for (int i = 1; i < 2 * n; i++) {
+                    sum2 += 4 * function.calculateFunction(bottomLimit + i * h);
+                    i++;
+                    sum2 += 2 * function.calculateFunction(bottomLimit + i * h);
+                }
+                i2 = (sum1 + function.calculateFunction(bottomLimit) - function.calculateFunction(topLimit) * h / 3);
+
+                if (Math.abs(i2 - i1) / 15 < accuracy) {
+                    result = i2;
+                    p = n;
+                    uac = Math.abs(i2 - i1) / 15;
+                    return 0;
+                }
+                if (n == 10000) {
+                    p = 0;
+                    return -1;
+                }
+            }
+        }
+        else {
+            result = 0;
+            p = 0;
+            return 1;
         }
     }
-
-
 }
